7a8944e6130a7d0e32f43f631bbc7676a67d5e5b
Yves Guilherme Lopo Ribeiro Lima <ext-yves.lima@bbts.com.br>
2020-06-26 16:17:35 -0300

    :tada:  Primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mqtt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-mqtt-client';
+}
